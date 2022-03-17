3021e5ad13f5e6c57e3ee8403270711d5166a90d
FlamurMustafa <flamurmustafa2001@gmail.com>
2022-03-17 16:04:12 +0100

    configured mysql and its connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+const pool = require("./config/db")
 const app = express();
 
 
 app.get('/', (req, res)=>{
     res.send("Hello World");
+});
+
+pool.query("select * from user", (err, result, fields )=>{
+    if(err){
+        console.log(err);
+    }
+    return console.log(result);
 });
 
 app.listen(3000);
