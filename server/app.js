15fb60a1c9d86c8433424449fdb8c638430a2b38
FlamurMustafa <flamurmustafa2001@gmail.com>
2022-04-19 13:49:22 +0200

    Added get route

--- server/app.js
+++ server/app.js
@@ -10,22 +10,40 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-
+//signup route
 app.post('/user/signup', (req, res)=>{
     const {username, name, lastname, is_professor, email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password, 5);
+    try{
     pool.query('INSERT into user(username, name, lastname, is_professor, email, password) VALUES '+
-    '(?,?,?,?,?,?)', [username, name, lastname, is_professor, email, hashedPassword ], (error, results)=>{
-        if(error) return res.json({error:error});
-        else(res.send(results));
+    '(?,?,?,?,?,?)', [username, name, lastname, is_professor, email, hashedPassword ], 
+    (error, results) => {
+        if(error) {
+            if(error.errno==1062) return res.status(409).json({message: 'Username already exists'});
+            return res.status(400).json({error});
+        }
+        return res.status(201);
     });
+} catch(e){
+    console.log(e);
+}
 });
 
-// pool.query("select * from user", (err, result, fields )=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     return console.log(result);
-// });
+app.get('/user/:email', (req, res)=>{
+    const email = req.params.email;
+
+    try{
+        pool.query('SELECT name, lastname, is_professor, email FROM user where email = ?', [email], (error, result)=>{
+            if(error) return res.status(400).json({error});
+            result = JSON.parse(JSON.stringify(result));
+            console.log(result);
+            return res.status(200).json(result);
+        })
+    } catch(e){
+        
+    }
+
+
+});
 
 app.listen(3000);
