5d2d3a4ab644147990a3a319d37e35b8361a4c73
FlamurMustafa <flamurmustafa2001@gmail.com>
2022-05-15 12:58:00 +0200

    configure main class

---
+++ src/main/java/com/example/ui/controllers/Main.java
@@ -0,0 +1,17 @@
+package com.example.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("log-in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
