a61fb26a019a6815b6514101a400a83a07085977
fortesamujaj <fortesamujaj@gmail.com>
2022-05-20 17:48:25 +0200

    Update Schedule Controller

--- src/main/java/com/example/ui/controllers/Schedules.java
+++ src/main/java/com/example/ui/controllers/Schedules.java
@@ -1,5 +1,31 @@
 package com.example.ui.controllers;
 
+import javafx.fxml.FXML;
+        import javafx.scene.control.TableColumn;
+        import javafx.scene.control.TableView;
+        import javafx.scene.control.TextField;
+
 public class Schedules {
+
+    @FXML
+    private TableColumn<?, ?> classID;
+
+    @FXML
+    private TableColumn<?, ?> endTime;
+
+    @FXML
+    private TableColumn<?, ?> roomID;
+
+    @FXML
+    private TableColumn<?, ?> scheduleID;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TableColumn<?, ?> startTime;
+
+    @FXML
+    private TableView<?> tableView;
 
 }
