039e8c10b654002fa8cd6753ebc0fd625b9933a5
FlamurMustafa <flamurmustafa2001@gmail.com>
2022-05-20 16:51:15 +0200

    added the functionality of some buttons

--- src/main/java/com/example/ui/controllers/Schedules.java
+++ src/main/java/com/example/ui/controllers/Schedules.java
@@ -1,5 +1,44 @@
 package com.example.ui.controllers;
 
-public class Schedules {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Schedules implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    private Button createBtn;
+
+
+
+    public void onCreateClicked(ActionEvent action) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/example/ui/views/add-appointment.fxml"));
+            Parent root = loader.load();
+            stage = (Stage) ((Node) action.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
