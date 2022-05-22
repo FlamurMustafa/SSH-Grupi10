bb29bb9b0cadae64a3ac65b14f2268b0f1b34120
arlindakastrati <74983643+arlindakastrati@users.noreply.github.com>
2022-05-22 19:27:55 +0200

    Adding controllers for addappcourse

--- src/main/java/com/example/ui/controllers/addAppointmentController.java
+++ src/main/java/com/example/ui/controllers/addAppointmentController.java
@@ -1,4 +1,11 @@
 package com.example.ui.controllers;
 
+import javafx.event.ActionEvent;
+
 public class addAppointmentController {
+    public void handlebuttonAdd(ActionEvent actionEvent) {
+    }
+
+    public void handlebuttonCancel(ActionEvent actionEvent) {
+    }
 }
