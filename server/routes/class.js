7011be1bfd1988bcccb689d60a672fa94089c721
fortesamujaj <fortesamujaj@gmail.com>
2022-05-23 16:22:39 +0200

    Small Changes To Class.js

--- server/routes/class.js
+++ server/routes/class.js
@@ -14,43 +14,6 @@ classRoute.use(
 );
 
 classRoute.post("/post", Auth, async (req, res) => {
-  const { datePicker, room_name, start_time, end_time, class_name } = req.body;
-  if(req.role_Id===0) return res.status(403).send({"Error":"You can't do this action"});
-  pool.query(
-    `SELECT * from schedule where end_time>"${req.body.start_time}" and start_time<"${req.body.end_time}" and room_id=${req.body.room_name}`,
-    (error, result) => {
-      if (error) return res.sendStatus(500);
-      if (result.length !== 0) {
-        return res.status(409).json({ error: "The schedule is already taken" });
-      }
-      pool.query(
-        `Select class_name from class where lecturer=${req.userId}`,
-        (err, result) => {
-          if (err) return res.status(err);
-          if(result.length===0) res.status(400).send({"error":"User does not have classes"});
-          insertIntoClass(result);
-        }
-      );
-      function insertIntoClass(result) {
-        pool.query(
-          "Insert into schedule(room_id, start_time, end_time, classid) values (?, ?, ?, ?)",
-          [
-            req.body.room_name,
-            req.body.start_time,
-            req.body.end_time,
-            result[0].class_name,
-          ],
-          (result, err) => {
-            if (err.affectedRows!==1) return res.status(500).json({ err });
-            return res.sendStatus(201);
-          }
-        );
-      }
-    }
-  );
-});
-
-/*classRoute.post("/post", Auth, async (req, res) => {
   if(req.role_Id===0) return res.status(403).send({"Error":"You can't do this action"});
   pool.query(
     `SELECT * from schedule where end_time>"${req.body.start_time}" and start_time<"${req.body.end_time}" and room_id=${req.body.room_id}`,
@@ -60,7 +23,7 @@ classRoute.post("/post", Auth, async (req, res) => {
         return res.status(409).json({ error: "The schedule is already taken" });
       }
       pool.query(
-        `Select classid from class where lecturer=${req.userId}`,
+        `Select classid, class_name from class where lecturer=${req.userId}`,
         (err, result) => {
           if (err) return res.status(err);
           if(result.length===0) res.status(400).send({"error":"User does not have classes"});
@@ -84,13 +47,14 @@ classRoute.post("/post", Auth, async (req, res) => {
       }
     }
   );
-});*/
+});
+
 
 classRoute.get("/", Auth, (req, res) => {
   if (req.role_Id === 0) {
     //fetch student's classes
     pool.query(
-      "select scheduleid, room_id, start_time, end_time, class_name from schedule join class on schedule.classid = class.classid join `student-classes` on `student-classes`.class_classid = class.classid join user on user.userid = `student-classes`.user_userid where user.userid = ?;",
+      "select scheduleid, room_id, start_time, end_time, classid from schedule join class on schedule.classid = class.classid join `student-classes` on `student-classes`.class_classid = class.classid join user on user.userid = `student-classes`.user_userid where user.userid = ?;",
       [req.userId],
       (errorq, result) => {
         if (errorq) res.status(500).send({ errorq });
@@ -99,7 +63,7 @@ classRoute.get("/", Auth, (req, res) => {
     );
   } else {
     pool.query(
-      "select scheduleid, room_id, start_time, end_time, class_name from schedule "+
+      "select scheduleid, room_id, start_time, end_time, classid from schedule "+
       +" join class on schedule.classid = class.classid "+
      +" where class.lecturer = ?",
       [req.userId],
