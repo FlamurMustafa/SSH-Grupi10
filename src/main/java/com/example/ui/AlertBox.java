0b68f90b241711df610e5bf5453ee2ca7e0ed66b
fortesamujaj <fortesamujaj@gmail.com>
2022-05-28 15:04:02 +0200

    Added the AlertBox Class

---
+++ src/main/java/com/example/ui/AlertBox.java
@@ -0,0 +1,33 @@
+package com.example.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.*;
+
+public class AlertBox {
+    public static void display(String title, String message){
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setWidth(300);
+        window.setHeight(200);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e->window.close());
+
+        VBox layout = new VBox(15);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
